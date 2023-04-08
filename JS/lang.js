0468a61765e8a59e26c68dd9492147c5c9994994
yan <ubelitis390871@gmail.com>
2023-04-08 19:20:59 +0300

    text language in CAT924.html

---
+++ JS/lang.js
@@ -0,0 +1,19 @@
+const langArr = {
+    "unit" :  {
+        "ru": "Централизованные системы смазки",
+        "en": "Centralized lubrication systems",
+        "ua": "Централізовані системи змащення",
+        "fr": "Systèmes de lubrification centralisée"
+    },
+    "chip": {
+        "ru": "графический чип",
+        "en": "graphics processing unit",
+        "ua": "графічний чіп",
+    },
+    "memory": {
+        "ru": "объем памяти",
+        "en": "memory size",
+        "ua": "oбсяг пам'яті",
+    },
+
+}
