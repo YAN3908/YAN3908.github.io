78d2da179579a57eed7bd7598892d64e0d20fcb6
yan <ubelitis390871@gmail.com>
2023-04-10 14:40:30 +0300

    connect file lang.js

--- JS/lang.js
+++ JS/lang.js
@@ -17,3 +17,56 @@ const langArr = {
     },
 
 }
+
+
+function changeBrouzerLanguage() {
+var userLang = navigator.language || navigator.userLanguage;
+<!--console.log(userLang);-->
+location.href = window.location.pathname + '#' + userLang.slice(0, 2);
+}
+        if (!window.location.hash) {
+            changeBrouzerLanguage();
+        }
+
+
+  $('.my-carousel').carousel().swipeCarousel({
+  // options here
+});
+const select = document.querySelector('select');
+const allLang = ['en', 'ru', 'uk', 'fr'];
+
+select.addEventListener('change', changeURLLanguage);
+
+// перенаправить на url с указанием языка
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+
+    hash = hash.substr(1);
+<!--    console.log(hash);-->
+        if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+        }
+        select.value = hash;
+
+var elements = document.querySelectorAll('.a-lang');
+elements.forEach(element => element.href += '#' + hash);
+
+
+        document.querySelector('title').innerHTML = langArr['unit'][hash];
+        document.documentElement.setAttribute('lang', hash)
+        document.querySelector('.lng-unit').innerHTML = langArr['unit'][hash];
+           for (let key in langArrText) {
+        let elem = document.querySelector('.' + key);
+        if (elem) {
+            elem.innerHTML = langArrText[key][hash];
+        }
+        }
+    }
+changeLanguage();
