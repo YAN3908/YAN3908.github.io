c53e1bb17a3a5cb73fe0edede443e572a02ae6fd
yan <ubelitis390871@gmail.com>
2023-04-10 13:43:48 +0300

    ad automatic change languish

--- JS/lang.js
+++ JS/lang.js
@@ -2,18 +2,18 @@ const langArr = {
     "unit" :  {
         "ru": "Централизованные системы смазки",
         "en": "Centralized lubrication systems",
-        "ua": "Централізовані системи змащення",
+        "uk": "Централізовані системи змащення",
         "fr": "Systèmes de lubrification centralisée"
     },
     "chip": {
         "ru": "графический чип",
         "en": "graphics processing unit",
-        "ua": "графічний чіп",
+        "uk": "графічний чіп",
     },
     "memory": {
         "ru": "объем памяти",
         "en": "memory size",
-        "ua": "oбсяг пам'яті",
+        "uk": "oбсяг пам'яті",
     },
 
 }
